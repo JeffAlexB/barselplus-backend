d7eb5594dd03543a49aea567b0f3a6bd7a74352c
jeffalexb <jeff.alex.burkey@gmail.com>
2025-07-01 08:49:35 +0200

    init project

---
+++ src/main/java/com/alex/barselplus_backend/BarselplusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alex.barselplus_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarselplusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarselplusBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alex/barselplus_backend/BarselplusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.barselplus_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarselplusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
