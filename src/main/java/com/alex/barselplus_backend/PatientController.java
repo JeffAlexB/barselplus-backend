2e508ccc2cb114c3bce4ad1465034f63dd1619cc
jeffalexb <jeff.alex.burkey@gmail.com>
2025-07-02 23:50:28 +0200

    implemented basic controller & tested endpoints

--- src/main/java/com/alex/barselplus_backend/PatientController.java
+++ src/main/java/com/alex/barselplus_backend/PatientController.java
@@ -1,4 +1,26 @@
 package com.alex.barselplus_backend;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path="/api/patients")
 public class PatientController {
+    private final PatientService patientService;
+
+    @Autowired
+    public PatientController(PatientService patientService) {
+        this.patientService = patientService;
+    }
+
+    @GetMapping(path ="/{nationalId}")
+    public PatientDTO findPatientByNationalId(@PathVariable Long nationalId) {
+        //System.out.println("Fetching patient with nationalID: " + nationalId); //debug
+        return patientService.findPatientByNationalID(nationalId);
+    }
+
+    @GetMapping("/testing")
+    public String testEndpoint() {
+        return "Controller is working?";
+    }
 }
