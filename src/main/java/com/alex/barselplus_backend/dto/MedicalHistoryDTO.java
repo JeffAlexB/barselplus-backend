2ade17a4dbdcdd1db6b791ccaa3d71abe9d015fa
jeffalexb <jeff.alex.burkey@gmail.com>
2025-07-18 18:00:21 +0200

    added suggested medhistory DTO

---
+++ src/main/java/com/alex/barselplus_backend/dto/MedicalHistoryDTO.java
@@ -0,0 +1,36 @@
+package com.alex.barselplus_backend.dto;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MedicalHistoryDTO {
+    private String chronicDiseases;
+    private String geneticConditions;
+
+    private String smokingUse;
+    private String snusUse;
+    private String alcoholUse;
+    private String otherDrugUse;
+
+    private String smokingWeek1;
+    private String snusWeek1;
+    private String alcoholWeek1;
+
+    private String smokingWeek36;
+    private String snusWeek36;
+    private String alcoholWeek36;
+
+    private String medications;
+    private String medList;
+    private Boolean drugAllergy;
+
+    private String folate; // TODO: check form if it should be a string or boolean 'yes'/'no'
+    private String notes;
+}
